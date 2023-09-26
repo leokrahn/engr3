1063a1a320b0731f4d1eab32493561ad466553d3
leokrahn <lkrahn75@charlottesvilleschools.org>
2023-09-26 15:11:38 -0400

    pwm added

--- potentiometer.py
+++ potentiometer.py
@@ -5,6 +5,12 @@
 import time
 import board
 from analogio import AnalogIn
+import time
+import pwmio
+import board
+
+pwm = pwmio.PWMOut(board.D3)
+
 
 potentiometer = AnalogIn(board.A0)  # potentiometer connected to A1, power & ground
 
@@ -14,3 +20,5 @@ while True:
 
     time.sleep(0.25)                   # Wait a bit before checking all again
     
+    pwm.duty_cycle = potentiometer.value
+    time.sleep(0.1)

55af9ae42c53a27dbebb6c34e1546bb9a2077343
leokrahn <lkrahn75@charlottesvilleschools.org>
2023-09-26 14:26:57 -0400

    kjghfd

---
+++ potentiometer.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2018 Anne Barela for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+from analogio import AnalogIn
+
+potentiometer = AnalogIn(board.A0)  # potentiometer connected to A1, power & ground
+
+while True:
+
+    print((potentiometer.value,))      # Display value
+
+    time.sleep(0.25)                   # Wait a bit before checking all again
+    

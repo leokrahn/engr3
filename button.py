bdacfe5af2230c9c9e91acde59d943b207415b5d
leokrahn <lkrahn75@charlottesvilleschools.org>
2023-10-24 14:54:49 -0400

    started photointerrupter documenting

--- button.py
+++ button.py
@@ -1,16 +1,45 @@
 import time
 import board
-from digitalio import DigitalInOut, Direction, Pull
+import adafruit_hcsr04
+import neopixel
+import simpleio
 
-btn = DigitalInOut(board.D13)
-btn.direction = Direction.INPUT
-btn.pull = Pull.UP
+NUMPIXELS = 1  # Update this to match the number of LEDs.
+SPEED = 0.1  # Increase to slow down the rainbow. Decrease to speed it up.
+BRIGHTNESS = 0.2  # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
+PIN = board.NEOPIXEL  # This is the default pin on the 5x5 NeoPixel Grid BFF.
+
+pixels = neopixel.NeoPixel(PIN, NUMPIXELS, brightness=BRIGHTNESS, auto_write=False)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D4, echo_pin=board.D3)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 while True:
-    if not btn.value:
-        print("BTN is down")
-    else:
-        #print("BTN is up")
-        pass
-
-    time.sleep(0.1) # sleep for debounce
+    try:
+        print((sonar.distance))
+        time.sleep(0.1)
+        if (sonar.distance < 5):
+            pixels.fill(RED)
+            pixels.show()
+            time.sleep(0.1)
+        elif (5 < sonar.distance < 20):
+            x = simpleio.map_range((sonar.distance),5,20,0,255)
+            pixels.fill((255-x, 0, x))
+            pixels.show()
+            time.sleep(0.1)
+        elif (sonar.distance == 20):
+            pixels.fill(BLUE)
+            pixels.show()
+            time.sleep(0.1)
+        elif (20 < sonar.distance < 35):
+            x = simpleio.map_range((sonar.distance),20,35,0,255)
+            pixels.fill((0, x, 255-x))
+            pixels.show()
+            time.sleep(0.1)
+        elif (sonar.distance > 35):
+            pixels.fill(GREEN)
+            pixels.show()
+            time.sleep(0.1)
+    except RuntimeError:
+        print("Retrying!")
